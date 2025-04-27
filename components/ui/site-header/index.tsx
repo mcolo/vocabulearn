ba3baa37c5b09ab1118c615a1b8b7a3d0bf800d7
Max Miracolo <max.miracolo@gmail.com>
2025-04-27 09:59:39 -0400

    move header from pages to layout

---
+++ components/ui/site-header/index.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { signOut } from "@/app/actions/auth";
+import { Button } from "../button";
+import { BookOpen, LogIn } from "lucide-react";
+import Link from "next/link";
+import { useAuth } from "@/components/auth-provider";
+
+export default function SiteHeader() {
+  const { user } = useAuth()
+  
+  async function handleSignOut() {
+    await signOut()
+  }
+
+  /**
+   * 
+   * lists-content
+   * profile-content
+   * list-detail-content
+   * learn-content
+   * 
+   */
+  return (
+    <>
+    <header className="border-b">
+      <div className="container flex items-center justify-between py-4">
+        <div className="flex items-center gap-2">
+          <BookOpen className="h-6 w-6" />
+          <Link href="/" className="text-xl font-bold">
+            VocabVault
+          </Link>
+        </div>
+        <nav className="hidden md:flex items-center">
+          <Link href="/about">
+            <Button variant="ghost">
+              About
+            </Button>
+          </Link>
+          {user === null &&
+            <Link href="/login">
+              <Button>
+                <LogIn className="mr-2 h-4 w-4" />
+                Sign In
+              </Button>
+            </Link>
+          }
+          {user !== null &&
+            <>
+              <Link href="/lists">
+                <Button variant="ghost">
+                  My Lists
+                </Button>
+              </Link>
+              <Link href="/learn">
+                <Button variant="ghost">
+                  Learn
+                </Button>
+              </Link>
+              <Link href="/profile">
+                <Button variant="ghost">
+                  Profile
+                </Button>
+              </Link>
+              <Button className="ml-4" variant="outline" onClick={handleSignOut}>
+                Sign Out
+              </Button>
+            </>
+          }
+        </nav>
+      </div>
+    </header>
+    </>
+  )
+}
